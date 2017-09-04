0bd2c1829055996412710497cc3f4610362daf16
Jonas Schievink <jonasschievink@gmail.com>
2017-09-04 21:13:55 +0200

    fish: Autoimport pip.fish

---
+++ fish/.config/fish/functions/pip.fish
@@ -0,0 +1,11 @@
+function pip
+    if contains "install" $argv
+        if not contains -- "--user" $argv
+            echo "`pip install` without `--user` denied. Use `pacman` to install system packages instead!"
+            exit 1
+        end
+    end
+
+    command pip $argv
+end
+
