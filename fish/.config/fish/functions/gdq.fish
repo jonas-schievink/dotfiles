72bc1e2bc7501b33b477adc551b0cb239b826218
Jonas Schievink <jonasschievink@gmail.com>
2019-06-27 13:48:23 +0200

    Use "best" quality instead of a specific one

--- fish/.config/fish/functions/gdq.fish
+++ fish/.config/fish/functions/gdq.fish
@@ -1,4 +1,4 @@
 function gdq
-    streamlink --player mpv -l warning https://www.twitch.tv/gamesdonequick 1080p60 -a "--profile=gpu-hq {filename}"
+    streamlink --player mpv -l warning https://www.twitch.tv/gamesdonequick best -a "--profile=gpu-hq {filename}"
 end
 
